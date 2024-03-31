ae5c4fd6be9b6d30c8bcb79492d13ae59defa86a
Sumedha Bandarigoda <90858067+Sumeda98@users.noreply.github.com>
2024-03-31 13:30:09 +0530

    style the active link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+    reactStrictMode: false,
+  }
+  
+  module.exports = nextConfig
+  
